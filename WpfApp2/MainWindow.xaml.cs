0c3a596bd28c93bbc91c7e7e11fca4a349a54ed8
Tomus_kd35 <Tomus_kd35@student.itstep.org>
2024-11-25 17:44:52 +0200

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }        
+        private void OnCalculeteClick(object sender, RoutedEventArgs e)
+        {
+            bool width = !double.TryParse(WidthTextBox.Text,out double widthvalue) || widthvalue <= 0;
+            bool height = !double.TryParse(HeighTextBox.Text, out double heightvalue) || heightvalue <= 0;
+
+            if (width || height) {
+                CalculateText.Visibility = Visibility.Hidden;
+
+                label1.Foreground = Brushes.Red;
+                label2.Foreground = Brushes.Red;
+                ErrorText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                label1.Foreground = Brushes.Black;
+                label2.Foreground = Brushes.Black;
+                ErrorText.Visibility = Visibility.Hidden;
+
+                double sum = widthvalue * heightvalue;
+                CalculateText.Visibility = Visibility.Visible;
+                CalculateText.Content = sum;
+            }
+        }
+    }
+}
